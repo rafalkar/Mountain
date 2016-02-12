b7955996ddfe2a8b3240642fa9276ebe453230e5
rafalkar <rakar@wp.pl>
2016-02-12 21:17:55 +0100

    Mountain

---
+++ main-kopia.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+ // proba Github
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Witojcie \n";
+    cout << "w Tatrach \n";
+    return 0;
+}
