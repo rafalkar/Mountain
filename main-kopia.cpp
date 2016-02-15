bc90edff662125c3458ad8cc4e2494e771a52982
rafalkar <rakar@wp.pl>
2016-02-15 23:06:30 +0100

    moze

--- main-kopia.cpp
+++ main-kopia.cpp
@@ -4,8 +4,14 @@ using namespace std;
  // proba Github
 int main()
 {
+    int s;
     cout << "Hello world!" << endl;
     cout << "Witojcie \n";
     cout << "w Tatrach \n";
+    cout << (char(91)) << (char(66)) << endl;
+    for (s=0; s<=255; s++)
+    {
+        cout << s << "=" <<char(s) << "," << endl;
+    }
     return 0;
 }
